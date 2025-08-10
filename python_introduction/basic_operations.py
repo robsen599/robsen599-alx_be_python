33ff6cdd9c8fcdf22d39939474b9227a160f0f1a
Robsen <robertabdena@gmail.com>
2025-08-09 19:50:59 -0700

    Moved basic_operations.py into python_introduction directory

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,12 +1,10 @@
-number1 = 10
-number2 = 5
+def main():
+    number1 = 10
+    number2 = 5
 
-# Perform arithmetic operations
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
+    print("Addition:", number1 + number2)
+    print("Subtraction:", number1 - number2)
+    print("Multiplication:", number1 * number2)
 
-# Print results
-print(f"Addition of {number1} and {number2} is {addition}")
-print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}")
+if __name__ == "__main__":
+    main()
