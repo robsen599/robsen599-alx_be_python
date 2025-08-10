4ad48632fdad8bad9d4f494972b5be0d19774688
Robsen <robertabdena@gmail.com>
2025-08-09 19:25:26 -0700

    Final: rubric-compliant arithmetic script

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+def main():
+    number1 = 10
+    number2 = 5
+
+    print("Addition:", number1 + number2)
+    print("Subtraction:", number1 - number2)
+    print("Multiplication:", number1 * number2)
+
+if __name__ == "__main__":
+    main()
